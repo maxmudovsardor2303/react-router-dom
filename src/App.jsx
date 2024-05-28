7242431a33ae09fd0c84995ee0591aa20c4eeff4
Your Name <you@example.com>
2024-05-28 12:38:06 +0500

    bvchjdbchj

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import { Routes, Route } from "react-router-dom";
-import Login from "./components/login/login";
-import Cars from "./components/cars/cars";
-const App = () =>{
-    return(
-        <>
-        <h1 className="text-center">Learn react-router-dom</h1>
-        <Routes>
-            <Route path="" element={<Login/>}/>
-            <Route path="cars" element={<Cars/>}/>
-        </Routes>
-        </>
-    )
+import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Routes, Route } from 'react-router-dom'
+import Login from './components/login/login'
+import Cars from './components/cars/cars'
+import SingleCar from './components/single-car/single-car';
+
+function App() {
+
+  return (
+    <>
+    <h1 className='text-center'>Learn react-router-dom</h1>
+     <Routes>
+      <Route path='' element={<Login/>}/>
+      <Route path='cars' element={<Cars/>}/>
+      <Route path='cars' element={<Cars/>}/>
+     </Routes>
+    </>
+  )
 }
+
 export default App
--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,46 +1,42 @@
-
-
 import { useState } from "react"
-import { useNavigate } from "react-router-dom"
 import "./login.css"
-
+import { useNavigate } from "react-router-dom"
 const Login = () => {
-  const [form, setForm] = useState ({})
-  const navigate = useNavigate()
-  const handleChange=(event)=>{
-    const {value, name} = event.target
-    setForm ({...form, [name]:value})
-
-  }
-  const handleSubmit=(e)=>{
-    e.preventDefault();
-    const {username, password} =form
-    if(username === "admin" && password === '123'){
-      navigate("/cars")
-    }else{
-      alert("404")
+    const [form, setForm] = useState({})
+    const navigate = useNavigate()
+    const handleChange=(event)=>{
+        const {value, name} = event.target
+        setForm({...form, [name]:value})
     }
-    
-
-  }
+    const handleSubmit =(e)=>{
+        e.preventDefault();
+        const {usarname, password} = form
+        if(usarname === "admin" && password === "123"){
+            navigate("/cars")
+        }else{
+            alert('404')
+        }
+    }
   return (
-    <div className="container">
-      <div className="row mt-2">
-        <div className="col-md-6 offset-3">
-        <div className="card">
-          <div className="card-header">
-            <h1 className="text-center">Login</h1>
-          </div>
-          <div className="card-body">
-          <form id="submit" onSubmit={handleSubmit}>
-            <input type="text" placeholder="username" onChange={handleChange} className="form-control my-2" name="username"  />
-            <input type="password" placeholder="password" onChange={handleChange} className="form-control my-2" name="password"  />
-          </form>
-          </div>
-          <div className="card-footer">
-            <button type="submit" form="submit" className="btn btn-success">login</button>
-          </div>
-        </div>
+    <div>
+      <div className="container">
+        <div className="row mt-2">
+            <div className="col-md-6 offset-3">
+               <div className="card">
+                <div className="card-header">
+                    <h1 className="text-center">Login</h1>
+                </div>
+                <div className="card-body">
+                <form id="submit" onSubmit={handleSubmit}>
+                    <input onChange={handleChange} type="text" placeholder="usarname" className="form-control my-2" name="usarname"/>
+                    <input onChange={handleChange} type="password" placeholder="password" className="form-control my-2" name="password"/>
+                </form>
+                </div>
+                <div className="card-footer">
+                    <button type="submit" form="submit" className="btn btn-success offset-5">Login</button>
+                </div>
+               </div>
+            </div>
         </div>
       </div>
     </div>
@@ -48,3 +44,4 @@ const Login = () => {
 }
 
 export default Login
+
